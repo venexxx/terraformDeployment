6c37516c8b58b71c9ae15641825e2234100502eb
venexxx <92607605+venexxx@users.noreply.github.com>
2024-02-12 18:43:43 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,83 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.89.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = "West Europe"
+}
+
+resource "azurerm_service_plan" "service_plan" {
+  name                = "contact-book-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "web_app" {
+  name                = "contact-webapp-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.service_plan.location
+  service_plan_id     = azurerm_service_plan.service_plan.id
+
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.mssqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.mssqldatabase.name};User ID=${azurerm_mssql_server.mssqlserver.administrator_login};Password=${azurerm_mssql_server.mssqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+}
+
+resource "azurerm_mssql_server" "mssqlserver" {
+  name                         = "mssqlserver${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "yoanligov"
+  administrator_login_password = "thisIsKat11"
+}
+
+resource "azurerm_mssql_database" "mssqldatabase" {
+  name         = "mssqldatabase${random_integer.ri.result}"
+  server_id    = azurerm_mssql_server.mssqlserver.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  sku_name     = "S0"
+}
+
+resource "azurerm_mssql_firewall_rule" "mssql_firewall" {
+  name             = "mssqldirewall${random_integer.ri.result}"
+  server_id        = azurerm_mssql_server.mssqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+
+resource "azurerm_app_service_source_control" "app_service" {
+  app_id                 = azurerm_linux_web_app.web_app.id
+  repo_url               = "https://github.com/venexxx/spring-web-23"
+  branch                 = "main"
+  use_manual_integration = true
+}
